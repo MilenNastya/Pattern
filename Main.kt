40e53ab73f66768cff1309919c8d990ac3a2bee8
Milen <148059423+MilenNastya@users.noreply.github.com>
2024-09-23 00:54:01 +0300

    Add files via upload
    1.2 Создала  6 объектов из отдельного класса main.

--- Main.kt
+++ Main.kt
@@ -1,6 +1,10 @@
 fun main() {
-    val student = Student("Милен","Настя","Романовна");
-    println(student.name);
-    student.name = "Илья";
-    println(student.name);
+    val students = mutableListOf<Student>();
+    students.add(Student("Милен", "Настя", "Романовна"));
+    students.add(Student("Кривошеев", "Илья", "Владимирович", studentTelegram = "@IlyaVladimirovich00"));
+    students.add(Student("Миленченко", "Анастасия", "Романовна", studentPhone  = "+79181130942"));
+    students.add(Student("Лыкова", "Каролина", "Дмитриевна", studentEmail = "karolina@gmail.com"));
+    students.add(Student("Малышев", "Дмитрий", "Араратович", studentTelegram = "@dmmvrs", studentPhone = "+79298491219"));
+    students.add(Student("Иванов", "Иван", "Иваныч"));
+    students.forEach { it: Student -> println(it) };
 }
