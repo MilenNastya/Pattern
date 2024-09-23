ab15d4931b12c1b024aec025bc419f250ca67804
Milen <148059423+MilenNastya@users.noreply.github.com>
2024-09-23 13:21:50 +0300

    1.6 Валидация телефонного номера

--- Main.kt
+++ Main.kt
@@ -4,7 +4,7 @@ fun main() {
     students.add(Student("Кривошеев", "Илья", "Владимирович", Telegram = "@IlyaVladimirovich00"));
     students.add(Student("Миленченко", "Анастасия", "Романовна", Phone  = "+79181130942"));
     students.add(Student("Лыкова", "Каролина", "Дмитриевна", Email = "karolina@gmail.com"));
-    students.add(Student("Малышев", "Дмитрий", "Араратович", Telegram = "@dmmvrs", Phone = "+79298491219"));
+    students.add(Student("Малышев", "Дмитрий", "Араратович", Telegram = "@dmmvrs", Phone = "+79298491211"));
     students.add(Student("Иванов", "Иван", "Иваныч"));
     students.add(Student(hashMapOf(Pair("name","Настя"),Pair("surname","Милен"),Pair("patronymic","Романовна"))));
     students.forEach { it: Student -> println(it) };
--- Student.kt
+++ Student.kt
@@ -2,30 +2,39 @@ data class Student(
     var surname:String,
     var name:String,
     var patronymic:String,
-    var id:Int?= autoGenerateId(),
+    var id:Int= autoGenerateId(),
     var Phone:String?=null,
     var Telegram:String?=null,
     var Email:String?=null,
-    var gitHub:String?=null,
+    var gitHub:String?=null
 
 ) {
+    init {
+        require(
+            Student.isValidPhone(this.Phone),
+            { "Valib Phone" }
+            );
+        }
+
     companion object {
         var classId: Int = 0;
         fun autoGenerateId(): Int {
             classId += 1;
             return classId;
         }
-    }
-    // Конструктор через HashMap
-    constructor(studentArgs: HashMap<String, Any?>) : this(
-        studentArgs["surname"]?.toString() ?: "",
-        studentArgs["name"]?.toString() ?: "",
-        studentArgs["patronymic"]?.toString() ?: "",
-        studentArgs["id"] as? Int ?: autoGenerateId(),
-        studentArgs["Phone"]?.toString(),
-         studentArgs["Telegram"]?.toString(),
-         studentArgs["Email"]?.toString(),
-        studentArgs["gitHub"]?.toString()
-    )
+       fun isValidPhone(phone: String?): Boolean {
+           return phone?.matches(Regex("\\+7\\d{10}")) ?: true;
+       }}
+    // Конструктор через hasmpam класса
+    constructor(studentArgs: HashMap<String,Any?>) : this(id = studentArgs.getOrDefault("id", autoGenerateId()) as Int,
+        surname = studentArgs["surname"].toString(),
+        name = studentArgs["name"].toString(),
+        patronymic = studentArgs["patronymic"].toString(),
+        Phone = studentArgs.getOrDefault("Phone",null) as String?,
+        Telegram = studentArgs.getOrDefault("Telegram",null).toString(),
+        Email = studentArgs.getOrDefault("Email",null).toString(),
+        gitHub = studentArgs.getOrDefault("gitHub",null).toString()) {}
+
 }
+
 
