6f2d2fc31aedd569eec2e8c1364785901e07ca95
Milen <148059423+MilenNastya@users.noreply.github.com>
2024-09-23 10:07:19 +0300

    1.3 Избежала дублирования кода в конструкторе, геттере и сеттере при помощи изменения класса в data class

--- Main.kt
+++ Main.kt
@@ -1,10 +1,10 @@
 fun main() {
     val students = mutableListOf<Student>();
     students.add(Student("Милен", "Настя", "Романовна"));
-    students.add(Student("Кривошеев", "Илья", "Владимирович", studentTelegram = "@IlyaVladimirovich00"));
-    students.add(Student("Миленченко", "Анастасия", "Романовна", studentPhone  = "+79181130942"));
-    students.add(Student("Лыкова", "Каролина", "Дмитриевна", studentEmail = "karolina@gmail.com"));
-    students.add(Student("Малышев", "Дмитрий", "Араратович", studentTelegram = "@dmmvrs", studentPhone = "+79298491219"));
+    students.add(Student("Кривошеев", "Илья", "Владимирович", Telegram = "@IlyaVladimirovich00"));
+    students.add(Student("Миленченко", "Анастасия", "Романовна", Phone  = "+79181130942"));
+    students.add(Student("Лыкова", "Каролина", "Дмитриевна", Email = "karolina@gmail.com"));
+    students.add(Student("Малышев", "Дмитрий", "Араратович", Telegram = "@dmmvrs", Phone = "+79298491219"));
     students.add(Student("Иванов", "Иван", "Иваныч"));
     students.forEach { it: Student -> println(it) };
 }
