411b3ffee5bb6d21b5d74b852c0e89889790ffbc
Milen <148059423+MilenNastya@users.noreply.github.com>
2024-12-15 20:29:29 +0300

    ЛР2.Задание 2.Добавлен класс StudentShort

--- Main.kt
+++ Main.kt
@@ -1,22 +1,33 @@
+
+
+import Student
+import StudentShort
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val students = mutableListOf<Student>();
-    students.add(Student("Aaaaa","Bbbbbb","Cccccc"));
-    students.add(Student("Aabbb-Abbb","Bbbbbb","Cccccc", telegramValue = "@elelelelele"));
-    students.add(Student("Aaabbb","Bbb-Cbbb","Cccccc", gitHubValue = "eeefefefef"));
-    students.add(Student("A-B","Bbbbbb","Cccccc", emailValue = "kk@gmial.com"));
-    students.add(Student("B","Vbbbbb","Cccccc", phoneNumberValue = "+79889889898"));
-    students.add(Student(hashMapOf(Pair("name","E"),Pair("surname","Bbbbbb"),Pair("patronymic","Cccccc"))));
-    students.forEach { it: Student -> println(it) };
-
-    println(Student(hashMapOf(Pair("name","E"),Pair("surname","Bbbbbb"),Pair("patronymic","Cccccc"))).validate())
+//    val students = mutableListOf<Student>();
+//    students.add(Student("Aaaaa","Bbbbbb","Cccccc"));
+//    students.add(Student("Aabbb-Abbb","Bbbbbb","Cccccc", telegramValue = "@elelelelele"));
+//    students.add(Student("Aaabbb","Bbb-Cbbb","Cccccc", gitHubValue = "eeefefefef"));
+//    students.add(Student("A-B","Bbbbbb","Cccccc", emailValue = "kk@gmial.com"));
+//    students.add(Student("B","Vbbbbb","Cccccc", phoneNumberValue = "+79889889898"));
+//    students.add(Student(hashMapOf(Pair("name","E"),Pair("surname","Bbbbbb"),Pair("patronymic","Cccccc"))));
+//    students.forEach { it: Student -> println(it) };
+//
+//    println(Student(hashMapOf(Pair("name","E"),Pair("surname","Bbbbbb"),Pair("patronymic","Cccccc"))).validate())
+//    println(Student("A-B","Bbbbbb","Cccccc", emailValue = "kk@gmial.com", gitHubValue = "eeefefefef").validate());
 
     val stud = Student("A-B","Bbbbbb","Cccccc", emailValue = "kk@gmial.com", gitHubValue = "eeefefefef", phoneNumberValue = "+79885556677");
-    stud.setContacts(hashMapOf(Pair("email","kkkkk@gmial.com"),Pair("telegram","@elelelel"),Pair("gitHub",null)));
+//    stud.setContacts(hashMapOf(Pair("email","kkkkk@gmial.com"),Pair("telegram","@elelelel"),Pair("gitHub",null)));
+//    println(stud)
+//    stud.name = "Abbbbcddd";
+    val data = Student.parseString("Student(id:4,surname:A,name:Bbbbbb,patronymic:Cccccc,phoneNumber:+79885556677,email:kk@gmial.com,telegram:,gitHub:eeefefefef)")
+    println(data)
     println(stud)
-    stud.name = "Abbbbcddd";
-    stud.phoneNumber="+79885556671"
-    println(stud)
+    println(Student(data))
+    println(Student(Student.parseString("Student(id:aaaa,surname:A,name:Bbbbbb,patronymic:Cccccc,phoneNumber:+79885556677,email:kk@gmial.com,telegram:,gitHub:eeefefefef)")))
     println(stud.getInfo())
+    println(StudentShort(4,stud.getInfo()))
+    println(StudentShort(stud))
 }
--- Student.kt
+++ Student.kt
@@ -1,59 +1,67 @@
-import kotlin.reflect.KFunction1
+import kotlin.math.max
 
 class Student(
     surnameValue: String,
     nameValue: String,
     patronymicValue: String,
+    idValue:Int = autoGenerateId(),
     phoneNumberValue: String?=null,
     telegramValue: String?=null,
     emailValue: String?=null,
-    gitHubValue: String?=null) {
-    var surname: String = surnameValue
-        set(value: String) {
+    gitHubValue: String?=null)
+{
+    var surname:String=surnameValue
+        set(value:String) {
             validatorSurname(value)
             field = value;
         };
-    var name: String = nameValue
-        set(value: String) {
+    var name:String=nameValue
+        set(value:String) {
             validatorName(value)
             field = value;
         };
-    var patronymic: String = patronymicValue
-        set(value: String) {
+    var patronymic:String=patronymicValue
+        set(value:String) {
             validatorPatronymic(value)
             field = value;
         };
-    var id: Int = autoGenerateId();
-    var phoneNumber: String? = phoneNumberValue
-        set(value: String?) {
-            if (this.checkValueAndPropertyNotNull(value, field)) {
+    var phoneNumber:String?=phoneNumberValue
+        set(value:String?) {
+            if(this.checkValueAndPropertyNotNull(value,field)) {
                 validatorPhoneNumber(value)
                 field = value;
             }
         };
-    var telegram: String? = telegramValue
-        set(value: String?) {
-            if (this.checkValueAndPropertyNotNull(value, field)) {
+    var telegram:String?=telegramValue
+        set(value:String?) {
+            if(this.checkValueAndPropertyNotNull(value,field)) {
                 validatorTelegram(value)
                 field = value;
             }
         };
-    var email: String? = emailValue
-        set(value: String?) {
-            if (this.checkValueAndPropertyNotNull(value, field)) {
+    var email:String?=emailValue
+        set(value:String?) {
+            if(this.checkValueAndPropertyNotNull(value,field)) {
                 validatorEmail(email)
                 field = value;
             }
         };
-    var gitHub: String? = gitHubValue
-        set(value: String?) {
-            if (this.checkValueAndPropertyNotNull(value, field)) {
+    var gitHub:String?=gitHubValue
+        set(value:String?) {
+            if(this.checkValueAndPropertyNotNull(value,field)) {
                 validatorGit(value)
                 field = value;
             }
         };
-
+    var id:Int= idValue
+        set(value:Int){
+            field = value
+            setMaxId(value)
+        }
     init {
+        if(!isValidId(this.id)){
+            this.id = autoGenerateId()
+        }
         validatorSurname(this.surname)
         validatorName(this.name)
         validatorPatronymic(this.patronymic)
@@ -63,135 +71,167 @@ class Student(
         validatorGit(this.gitHub);
     }
 
-    // Функция для валидации полей
-    private fun <T> validatorFunc(value: T, errorMessage: String, valudatorFunction: KFunction1<T, Boolean>) {
+    // Функция для валидации полей по регулярке
+    private fun <T>validatorFunc(value:T, errorMessage:String, valudatorFunction: (T)->Boolean){
         require(valudatorFunction(value)) { errorMessage }
     }
-
     // Валидатор гита
-    private fun validatorGit(gitHub: String?) = validatorFunc(gitHub, "Git must be a valid git", ::isValidGitHub)
+    private fun validatorGit(gitHub: String?) = validatorFunc(gitHub,"Git must be a valid git",::isValidGitHub)
 
     // Валидатор почты
-    private fun validatorEmail(email: String?) = validatorFunc(email, "Email must be a valid email", ::isValidEmail)
+    private fun validatorEmail(email: String?) = validatorFunc(email,"Email must be a valid email",::isValidEmail)
 
     // Валидатор телефона
-    private fun validatorPhoneNumber(phone: String?) =
-        validatorFunc(phone, "Phone must be a valid phone number", ::isValidPhone)
+    private fun validatorPhoneNumber(phone: String?) = validatorFunc(phone, "Phone must be a valid phone number",::isValidPhone)
 
     // Валидатор телеги
-    private fun validatorTelegram(telegram: String?) =
-        validatorFunc(telegram, "Telegram must be a valid telegram", ::isValidTelegram)
+    private fun validatorTelegram(telegram: String?) = validatorFunc(telegram, "Telegram must be a valid telegram",::isValidTelegram)
 
     // Валидатор фамилии
-    private fun validatorSurname(surname: String) =
-        validatorFunc(surname, "Surname must be a valid surname", ::isValidSurname)
+    private fun validatorSurname(surname: String) = validatorFunc(surname, "Surname must be a valid surname",::isValidSurname)
 
     // Валидатор имени
-    private fun validatorName(name: String) = validatorFunc(name, "Name must be a valid name", ::isValidName)
+    private fun validatorName(name: String) = validatorFunc(name, "Name must be a valid name",::isValidName)
 
     // Валидатор отчества
-    private fun validatorPatronymic(patronymic: String) =
-        validatorFunc(patronymic, "Patronymic must be a valid patronymic", ::isValidPatronymic)
+    private fun validatorPatronymic(patronymic: String) = validatorFunc(patronymic, "Patronymic must be a valid patronymic",::isValidPatronymic)
+
 
     //Проверка наличия гита
-    private fun gitExist() = this.gitHub != null
+    private fun gitExist() = this.gitHub!=null
 
     //Проверка наличия второго контакта
-    private fun contactExist() = this.email != null || this.telegram != null || this.phoneNumber != null
+    private fun contactExist() = this.email!=null || this.telegram!=null || this.phoneNumber!=null
 
     //Проверка наличия гита и 1 из контактов
     fun validate() = this.gitExist() && this.contactExist()
 
 
     //Метод set_contacts
-    fun setContacts(contacts: HashMap<String, String?>) {
-        this.phoneNumber = contacts.getOrDefault("phoneNumber", this.phoneNumber);
-        this.gitHub = contacts.getOrDefault("gitHub", this.gitHub);
-        this.email = contacts.getOrDefault("email", this.email)
-        this.telegram = contacts.getOrDefault("telegram", this.telegram);
+    fun setContacts(contacts:HashMap<String,String?>){
+        this.phoneNumber = contacts.getOrDefault("phoneNumber",this.phoneNumber);
+        this.gitHub = contacts.getOrDefault("gitHub",this.gitHub);
+        this.email = contacts.getOrDefault("email",this.email)
+        this.telegram = contacts.getOrDefault("telegram",this.telegram);
     }
+    private fun checkValueAndPropertyNotNull(value:String?,propertyValue:String?) = value==null && propertyValue!=null || value!=null
 
-    private fun checkValueAndPropertyNotNull(value: String?, propertyValue: String?) =
-        value == null && propertyValue != null || value != null
-
-    companion object {
+    companion object{
         // Автосоздание id
-        private var classId: Int = 0
-        private fun autoGenerateId(): Int {
-            classId += 1
+        private var classId:Int = 0
+        private var maxId:Int = 0;
+        private fun autoGenerateId():Int{
+            classId= maxId
+            maxId = maxId + 1;
             return classId
         }
-
+        private fun setMaxId(newId:Int?){
+            maxId = max(maxId,newId as Int);
+        }
         // Валидация телефона
         private fun isValidPhone(phone: String?): Boolean {
             return phone?.matches(Regex("\\+7\\d{10}")) ?: true
         }
 
         //Валидация фамилии
-        private fun isValidSurname(surname: String): Boolean {
+        private  fun isValidSurname(surname: String): Boolean {
             return surname.matches(Regex("^[A-Z][a-z]*(-([A-Za-z]?)[a-z]*)*$"))
         }
-
         //Валидация имени
-        private fun isValidName(name: String): Boolean {
+        private  fun isValidName(name: String): Boolean {
             return name.matches(Regex("^[A-Z][a-z]*(-([A-Za-z]?)[a-z]*)*$"))
         }
-
         //Валидация отчества
-        private fun isValidPatronymic(patronymic: String): Boolean {
+        private  fun isValidPatronymic(patronymic: String): Boolean {
             return patronymic.matches(Regex("^[A-Z][a-z]*(-([A-Za-z]?)[a-z]*)*$"))
         }
 
         //Валидация телеграм
-        private fun isValidTelegram(telegram: String?): Boolean {
+        private  fun isValidTelegram(telegram: String?): Boolean {
             return telegram?.matches(Regex("@(?=.{5,64})(?!_)(?!.*__)[a-zA-Z0-9_]+(?<![_.])")) ?: true
         }
 
         //Валидация почты
-        private fun isValidEmail(email: String?): Boolean {
+        private  fun isValidEmail(email: String?): Boolean {
             return email?.matches(Regex("^[a-zA-Z][a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]{2,}$")) ?: true
         }
 
         //Валидация гита
-        private fun isValidGitHub(gitHub: String?): Boolean {
+        private  fun isValidGitHub(gitHub: String?): Boolean {
             return gitHub?.let { !Regex("[$%#@&/?]").matches(it) } ?: true
         }
+
+        //Валидация id - дожили
+        private fun isValidId(id: Int?) = if(id==null) false else true;
+        //Парсер строки
+        private fun cutStudent(data:String) = data.split("^Student\\(".toRegex())[1].split("\\)$".toRegex())[0]
+        fun parseString(data:String):HashMap<String,Any?>{
+            val dataWithoutPrefix = cutStudent(data).split(',')
+            val hashData = HashMap<String,Any?>();
+            for (propertyValue in dataWithoutPrefix){
+                if(!propertyValue.matches("[a-zA-Z]{2,11}:.*".toRegex())){
+                    throw Exception("Неверный формат строки")
+                }
+                else{
+                    val (key,propertyVal) = propertyValue.split(":".toRegex());
+                    hashData.set(key,if(propertyVal=="") null else propertyVal);
+                }
+            }
+            println(hashData.getOrDefault("id",null).toString().toIntOrNull()==null)
+            return hashData
+        }
+
+        private fun formatPropertyOutput(propertyName:String,propertyValue: Any?) = if(propertyValue==null) "${propertyName}:" else "${propertyName}:${propertyValue}"
 
     }
 
     // Конструктор через hasmpam класса
-    constructor(studentArgs: HashMap<String, Any?>) : this(
-        surnameValue = studentArgs["surname"].toString(),
-        nameValue = studentArgs["name"].toString(),
+    constructor(studentArgs: HashMap<String,Any?>) : this(
+        surnameValue    = studentArgs["surname"].toString(),
+        nameValue       = studentArgs["name"].toString(),
         patronymicValue = studentArgs["patronymic"].toString(),
-        phoneNumberValue = studentArgs.getOrDefault("phoneNumber", null) as String?,
-        telegramValue = studentArgs.getOrDefault("telegram", null) as String?,
-        emailValue = studentArgs.getOrDefault("email", null) as String?,
-        gitHubValue = studentArgs.getOrDefault("gitHub", null) as String?
-    )
+        idValue         = if(studentArgs.getOrDefault("id",null).toString().toIntOrNull()==null) autoGenerateId() else (studentArgs["id"].toString().toInt()),
+        phoneNumberValue = studentArgs.getOrDefault("phoneNumber",null) as String?,
+        telegramValue = studentArgs.getOrDefault("telegram",null) as String?,
+        emailValue = studentArgs.getOrDefault("email",null) as String?,
+        gitHubValue = studentArgs.getOrDefault("gitHub",null) as String?)
+
+    //Конструктор парсер строки
+    constructor(data: String) : this(parseString(data))
+    private fun propertiesReturn() =
+        mapOf(
+            "id" to this.id,
+            "surname" to this.surname,
+            "name" to this.name,
+            "patronymic" to this.patronymic,
+            "phoneNumber" to this.phoneNumber,
+            "email" to this.email,
+            "telegram" to this.telegram,
+            "gitHub" to this.gitHub
+        )
 
     override fun toString(): String {
-        return "Student(id:${this.id},surname:${this.surname},name:${this.name},patronymic:${this.patronymic},phoneNumber:${this.phoneNumber},email:${this.email},telegram:${this.telegram},gitHub:${this.gitHub}"
+        var resultString = "Student("
+        for ((key,propValues) in propertiesReturn().entries){
+            resultString += "${formatPropertyOutput(key,propValues)},"
+        }
+        return resultString.dropLast(1).plus(")")
     }
 
-    constructor(input:String): this (input.split(" ")
-        [0],input.split(" ")[1],input.split(" ")
-        [2],input.split(" ").getOrNull(3),
-        input.split(" ").getOrNull(4),
-        input.split(" ").getOrNull(5),
-        input.split(" ").getOrNull(6))
-
     //Получаем краткую инфу
-    private fun getInitials() = "${this.surname} ${this.name[0].plus(".")} ${this.patronymic[0].plus(".")}"
-    private fun getOneContact() =
+    fun getInitials() = "${this.surname} ${this.name[0].plus(".")} ${this.patronymic[0].plus(".")}"
+    fun getOneContact() =
         when{
             this.phoneNumber!=null -> hashMapOf("phoneNumber" to this.phoneNumber)
             this.telegram!=null -> hashMapOf("telegram" to this.telegram)
             this.email!=null -> hashMapOf("email" to this.email)
             else -> null
         }
-    fun getInfo():String{
+    private fun getFormattedContactShort():String{
         val contact = getOneContact()
-        return "Initials:${this.getInitials()} Contact: ${contact?.keys?.first()}:${contact?.values?.first()}"
+        return if(contact?.keys!=null)formatPropertyOutput(contact.keys.first() as String,contact.values.first()) else ""
+    }
+    fun getInfo():String{
+        return "Initials:${this.getInitials()}, ${formatPropertyOutput("gitHub",this.gitHub)}, Contact: ${getFormattedContactShort()}"
     }
 }
